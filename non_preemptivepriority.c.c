08bd6508d7cc085d3b44a071e41a43626b9b9ccb
jaseem61 <54478100+jaseem61@users.noreply.github.com>
2019-11-07 07:10:48 +0530

    updated.

--- non_preemptivepriority.c.c
+++ non_preemptivepriority.c.c
@@ -1,79 +1,60 @@
 #include<stdio.h>
 void main()
 {
-    int bt[10],at[10],p[10],wt[10],tat[10],avg_wt,avg_tat,n,i,j,c[10],count=0,total_tat=0,total_wt=0,pid[10],position,temp,time,smallest;
-    printf("Enter the number of processes:");
-    scanf("%d",&n);
-    printf("Enter the arrival time:");
-    for(i=0;i<n;i++)
-    {
+	int n,count,i,at[10],bt[10],p[10],pid[10],c[10],smallest,time=0,wt[10],tat[10],total_wt=0,total_tat=0;
+	printf("\n\nnon_preemptive priority\n\n");
+  printf("Enter the number of procesess:\n");
+  scanf("%d",&n);
+  count=n;
+  printf("\nEnter the arrival time of the procesess:\n");
+  for(i=0;i<n;i++)
+  {
     scanf("%d",&at[i]);
+    c[i]=0;
     pid[i]=i+1;
-    }
-    printf("Enter the burst time:");
+  }
+  printf("\nEnter the burst time of the processes:\n");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&bt[i]);
+  }
+  printf("\nEnter the priority of the processes:\n");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&p[i]);
+  }
+  p[9]=999;
+  printf("\n\nGHANTT CHART\n\n");
+  while(count!=0)
+  { smallest=9;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&bt[i]);
+      if(at[i]<=time&&p[i]<p[smallest]&&c[i]==0)
+      {
+        smallest=i;
+      }
     }
-    printf("Enter the priority:");
-    for(i=0;i<n;i++)
+    if(smallest!=9)
     {
-        scanf("%d",&p[i]);
-        c[i]=0;
+      time=time+bt[smallest];
+      wt[smallest]=time-bt[smallest]-at[smallest];
+      tat[smallest]=time-at[smallest];
+      c[smallest]=1;
+      printf("%d---->",pid[smallest]);
+      count--;
     }
-    for(i=0;i<n;i++)
+    else
     {
-        position=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(at[j]<at[position])
-            {
-                position=j;
-            }
-        }
-        temp=at[position];
-        at[position]=at[i];
-        at[i]=temp;
-        temp=bt[position];
-        bt[position]=bt[i];
-        bt[i]=temp;
-        temp=p[position];
-        p[position]=p[i];
-        p[i]=temp;
-        temp=pid[position];
-        pid[position]=pid[i];
-        pid[i]=temp;
-    }
-    p[9]=999;
-    time=0;
-    printf("\nGhant chart is as shown:\n");
-    for(time=0;count!=n;time++)
-    {
-        smallest=9;
-        for(i=0;i<n;i++)
-        {
-        if(at[i]<=time&&p[i]<p[smallest]&&c[i]==0)
-        {
-            smallest=i;
-        }
-    	}
-        printf("%d-->",pid[smallest]);
-        count++;
-        c[smallest]=1;
-        wt[smallest]=time-at[smallest];
-        time=time+bt[smallest];
-        tat[smallest]=time-at[smallest];
-    }
-    for(i=0;i<n;i++)
-    {
-        total_wt=total_wt+wt[i];
-        total_tat=total_tat+tat[i];
-    }
-    printf("\nprocess_id\t\tat\t\tbt\t\twt\t\ttat");
-    for(i=0;i<n;i++)
-    {
-        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",pid[i],at[i],bt[i],wt[i],tat[i]);
-    }
-    printf("\naverage waiting time is: %f",(float)total_wt/n);
-    printf("\naverage turn around time is: %f",(float)total_tat/n);
+    	time++;
+	}
+  }
+  	printf("\nprocess_id\t\tat\t\tbt\t\twt\t\ttat");
+  for(i=0;i<n;i++)
+  {
+	printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",pid[i],at[i],bt[i],wt[i],tat[i]);
+    total_wt=total_wt+wt[i];
+    total_tat=total_tat+tat[i];
+  }
+  printf("\nThe average waiting time is %f",(float)total_wt/n);
+  printf("\nThe average turn around time is %f",(float)total_tat/n);
 }
